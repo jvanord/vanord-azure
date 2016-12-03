017681740aa5353ecafaacc1a7445000afabf040
jvanord <jvanord@indasysllc.com>
2016-12-02 22:51:52 -0500

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VanOrd_Azure.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return RedirectToAction("Display", "Resume", new { name = "Jay" });
+		}
+
+		public ActionResult About()
+		{
+			return Content("Why were you expecting an about page?");
+		}
+
+		public ActionResult Contact()
+		{
+			return Content("Why were you expecting a contact page?");
+		}
+	}
+}
---
+++ Controllers/ResumeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VanOrd_Azure.Controllers
+{
+	public class ResumeController : Controller
+	{
+		// GET: Resume
+		[Route("resume/{name}")]
+		public ActionResult Display(string name)
+		{
+			return View(name);
+		}
+
+		[Route("jay/resume")]
+		public ActionResult LegacyRedirect()
+		{
+			return RedirectToAction("Display", new { name = "Jay" });
+		}
+	}
+}
